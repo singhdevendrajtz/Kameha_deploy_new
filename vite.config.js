77c9f36eaa88eabd9de6c55c8191ee0985406b99
singhdevendrajtz <singhdevendrajtz@gmail.com>
2026-09-21 16:22:51 +0530

    initial

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Kameha_deploy_new/', // MUST match your GitHub repo name exactly
+  base: '/Kameha_deploy_new/', // Replace with your exact GitHub repo name
 })
